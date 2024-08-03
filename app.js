81026b144eae8120ab7f82b15fcbbab993eecc46
Andre Barros <andrebarros7201@gmail.com>
2024-08-03 12:33:33 +0100

    create session

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
+const expressSession = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
 
 const app = express();
 
@@ -7,6 +10,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 const indexRouter = require("./routes/indexRouter");
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
 
 app.use(indexRouter);
 
